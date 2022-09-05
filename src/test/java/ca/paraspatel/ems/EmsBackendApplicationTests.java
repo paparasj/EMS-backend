4f4d60a42d039837607aeb2a9c5928ae306bb60f
Paras Patel <paparasj@sheridancollege.ca>
2022-09-05 01:36:11 -0400

    First Commit

---
+++ src/test/java/ca/paraspatel/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.paraspatel.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
